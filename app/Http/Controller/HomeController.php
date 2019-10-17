8fb1ac9ded20ad4421cd141c7f04d560357272f3
tqrj <2816869721@qq.com>
2019-10-17 17:32:25 +0800

    gun

--- app/Http/Controller/HomeController.php
+++ app/Http/Controller/HomeController.php
@@ -48,7 +48,7 @@ class HomeController
      */
     public function pull():Response
     {
-        (string)$flag = exec("git");
+        (string)$flag = shell_exec("git");
 
         return Context()->getResponse()->withContent($flag);
     }
